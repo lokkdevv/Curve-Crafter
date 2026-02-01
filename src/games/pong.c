e8a34da6a44766c4134495bf348aab5394ec4721
lokk <223161425+lokkdevv@users.noreply.github.com>
2026-02-01 20:56:04 +0000

    Implement ball physics, collisions and UI tweaks
    Add bin/pong.zip and update src/games/pong.c to implement ball behavior and improve startup UI. Changes: add stdlib.h/time.h and BALL_SPEED; initialize ball random start direction; add paddle collision detection for both players, vertical bounds reflection, ball position updates and game reset when ball leaves play area; reposition and reset player positions when showing start message; add startup instruction text. Note: srand(time(NULL)) was also added inside the main loop (may be redundant).

--- src/games/pong.c
+++ src/games/pong.c
@@ -2,16 +2,30 @@
 
 #include "../input/win_input.h"
 #include "../renderer/win_renderer.h"
+#include <stdlib.h>
+#include <time.h>
 
 #else
 
 #include "../input/unix_input.h"
 #include "../renderer/unix_renderer.h"
+#include <stdlib.h>
+#include <time.h>
 
 #endif
 
+#define BALL_SPEED 1
+
 int main()
 {
+	// Set random starting Direction for the Ball //
+	COORD ball_dir = {0, 0};
+	srand(time(NULL));
+	ball_dir.X = rand() % 2;
+	if (ball_dir.X == 0) ball_dir.X = -BALL_SPEED;
+	else ball_dir.X = BALL_SPEED;
+	////////////////////////////////////////////////
+
 	char game_started = 0;
 
 	COORD player1_pos = {5, 10};
@@ -23,8 +37,8 @@ int main()
 	
 	while (running)
 	{
-		// Correct PLAYER2 x position
-		player2_pos.X = console_size.X - 5;
+		// Seed random number
+		srand(time(NULL));
 
 		// Exit if ESC pressed
 		if (exited()) break;
@@ -35,9 +49,16 @@ int main()
 		{
 			ball_pos.X = console_size.X / 2 - 1;
 			ball_pos.Y = console_size.Y / 2 - 1;
+
+			player1_pos.Y = console_size.Y / 2 - 1;
+			player2_pos.Y = console_size.Y / 2 - 1;
+			player2_pos.X = console_size.X - 5;
+
 			COORD text_pos = {ball_pos.X - ball_pos.X / 4, ball_pos.Y};
+			COORD text_pos_2 = {ball_pos.X - (ball_pos.X / 2 - ball_pos.X / 10), ball_pos.Y + 2};
 			clear_console(console_area);
 			draw_chars("Press 'SPACE' to start the game.", 32, text_pos);
+			draw_chars("Do not resize the terminal windows after pressing 'SPACE'.", 58, text_pos_2);
 		}
 		
 		else if (game_started)
@@ -46,13 +67,13 @@ int main()
 			if (is_key_pressed(KEY_Z)) player1_pos.Y -= 1;
 			if (is_key_pressed(KEY_S)) player1_pos.Y += 1;
 			//////////////////////////////////////
-	
+			
 			// PLAYER2 input /////////////////////
 			if (is_key_pressed(KEY_UP)) player2_pos.Y -= 1;
 			if (is_key_pressed(KEY_DOWN)) player2_pos.Y += 1;
 			//////////////////////////////////////
-	
-			// Bounds Checking ///////////////////
+			
+			// Player Bounds Checking ////////////
 			if (player1_pos.Y < 1) player1_pos.Y = 1;
 			if (player1_pos.Y > console_size.Y - 2) player1_pos.Y = console_size.Y - 2;
 			if (player2_pos.Y < 1) player2_pos.Y = 1;
@@ -67,6 +88,40 @@ int main()
 			COORD player2_pos_bottom = {player2_pos.X, player2_pos.Y + 1};
 			//////////////////////////////////////
 
+			// P1 Collision Detection ////////////
+			if (ball_dir.X < 0 && (ball_pos.X <= player1_pos.X) && (ball_pos.Y <= player1_pos_bottom.Y) && (ball_pos.Y >= player1_pos_top.Y))
+			{
+				if (ball_pos.Y < player1_pos.Y) ball_dir.Y = -1;
+				else if (ball_pos.Y > player1_pos.Y) ball_dir.Y = 1;
+				else ball_dir.Y = 0;
+				
+				ball_dir.X = -ball_dir.X;
+			}
+			//////////////////////////////////////
+
+			// P2 Collision Detection ////////////
+			else if (ball_dir.X > 0 && (ball_pos.X >= player2_pos.X) && (ball_pos.Y <= player2_pos_bottom.Y) && (ball_pos.Y >= player2_pos_top.Y))
+			{
+				if (ball_pos.Y < player2_pos.Y) ball_dir.Y = -1;
+				else if (ball_pos.Y > player2_pos.Y) ball_dir.Y = 1;
+				else ball_dir.Y = 0;
+
+				ball_dir.X = -ball_dir.X;
+			}
+			//////////////////////////////////////
+			
+			// Ball Bounds Checking //////////////
+			if (ball_pos.Y < 1) ball_dir.Y = -ball_dir.Y;
+			if (ball_pos.Y > console_size.Y - 2) ball_dir.Y = -ball_dir.Y;
+
+			if (ball_pos.X < 0 || ball_pos.X > console_size.X) game_started = 0;
+			//////////////////////////////////////
+
+			// Update Ball position //////////////
+			ball_pos.X += ball_dir.X;
+			ball_pos.Y += ball_dir.Y;
+			//////////////////////////////////////
+
 			clear_console(console_area);
 			
 			draw_char("@", ball_pos);
@@ -74,7 +129,7 @@ int main()
 			draw_char("#", player1_pos_top);
 			draw_char("#", player1_pos);
 			draw_char("#", player1_pos_bottom);
-	
+			
 			draw_char("#", player2_pos_top);
 			draw_char("#", player2_pos);
 			draw_char("#", player2_pos_bottom);
